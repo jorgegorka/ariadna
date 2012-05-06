732568126f00d7abcaab0892e562622f61bc82fa
Jorge Alvarez <jorge@alvareznavarro.es>
2012-05-06 19:30:36 +0100

    refactor Result initialize

--- spec/lib/ariadna/result_spec.rb
+++ spec/lib/ariadna/result_spec.rb
@@ -10,22 +10,28 @@ describe "Result" do
   end
 
   context "Results from a query" do
-    it "should save dimensions and metrics as attributes" do
+    it "save dimensions and metrics as attributes" do
       @results.first.country.should == "United States"
       @results.first.visits.should  == 94351
     end
 
-    it "should save general query information" do
+    it "save general query information" do
       @results.first.itemsPerPage.should == 5
       @results.first.query["start-date"].should == "2010-04-01"
     end
 
-    it "should return data" do
+    it "return data" do
       @results.size.should == 5
     end
 
     it "returns an array of results objects" do
       @results.first.class.should == Ariadna::Result
+    end
+
+    context "total headers" do
+      it "creates a total result attribute and populates it" do
+        @results.first.total_visits.should == "241543"
+      end
     end
   end
 
